ecc913c2e7650f9fe47f5975d6ccd2184f0c351c
tovehydmark <tovehydmark@gmail.com>
2022-05-09 10:52:45 +0200

    mock data books list

--- routes/books.js
+++ routes/books.js
@@ -1,1 +1,30 @@
 //Hantera böckerna med en global object array i denna publika mapp "books.js" 
+let libraryBooks = [{
+        "title": "Harry Potter",
+        "authorFirstName": "J.K.",
+        "authorLastName": "Rowling",
+        "numberOfPages": 900,
+        "onLoan": false
+    },
+    {
+        "title": "Where the Rain is Born",
+        "authorFirstName": "Anita",
+        "authorLastName": "Nair",
+        "numberOfPages": 297,
+        "onLoan": false
+    },
+    {
+        "title": "Tears of the Giraffe",
+        "authorFirstName": "Alexander",
+        "authorLastName": "McCall Smith",
+        "numberOfPages": 217,
+        "onLoan": false
+    },
+    {
+        "title": "The Kite Runner",
+        "authorFirstName": "Khaled",
+        "authorLastName": "Hosseini",
+        "numberOfPages": 371,
+        "onLoan": false
+    }
+]
