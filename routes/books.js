c473577270151c3b00276dda2a9ce4454b78aa62
tovehydmark <tovehydmark@gmail.com>
2022-05-11 14:10:53 +0200

    add new books to json file works

--- routes/books.js
+++ routes/books.js
@@ -7,69 +7,103 @@ var router = express.Router();
 var rand = require("random-key")
 
 
+const fs = require("fs")
+
+
+//ID in json file are not made up by some generator. Is there any way of using a generator to do that? 
+
 let backToStartPage = "<a href='http://localhost:3000/books/'>Tillbaka till startsidan</a>"
 
-//Gets all books in library and prints them on page
+
+
+//Gets books from books.json and prints them on page
 router.get('/', function (req, res) {
 
     let allBooks = "<h1>Bibliotekets alla böcker: </h1>"
     let addNewBook = "<a href='http://localhost:3000/books/addbook'>Lägg till en ny bok i systemet</a>"
 
-    libraryBooks.forEach(book => {
-
-        let lendingStatus;
-        if (book.onLoan == false) {
-            lendingStatus = "Boken är tillgänglig"
-        } else {
-            lendingStatus = "Boken är utlånad"
+    fs.readFile("books.json", function (err, data) {
+        if (err) {
+            console.log(err);
         }
 
-        allBooks += `
-        <article>
-        <h2>Titel: ${book.title}</h2>
-        <p>Lånestatus: ${lendingStatus}</p>
-        <a href="http://localhost:3000/books/bookinfo/${book.bookId}">Visa mer info om boken</a>
-      </article>
-        `
-    });
+        const books = JSON.parse(data);
 
-    res.send(allBooks + addNewBook)
+
+        books.forEach(book => {
+
+            let lendingStatus;
+
+            if (book.onLoan == false) {
+                lendingStatus = "Boken är tillgänglig"
+            } else {
+                lendingStatus = "Boken är utlånad"
+            }
+
+            allBooks += `
+                    <article>
+                    <h2>Titel: ${book.title}</h2>
+                    <p>Lånestatus: ${lendingStatus}</p>
+                    <a href="http://localhost:3000/books/bookinfo/${book.bookId}">Visa mer info om boken</a>
+                    </article>`
+
+
+        });
+
+        res.send(allBooks + addNewBook)
+
+    })
+
 })
+
 
 
 //Information on the specific book chosen is shown on separate url
 router.get('/bookinfo/:bookid', function (req, res) {
 
-    let showBook = libraryBooks.find((book) => book.bookId == req.params.bookid);
+    fs.readFile("books.json", function (err, data) {
 
-    if (!showBook) {
-        return res.send("Det finns ingen bok med id " + req.params.bookid)
-    }
+        const books = JSON.parse(data);
 
-    let chosenBook = `
-    <h2>Titel: ${showBook.title}</h2>
-    <p>Författare: ${showBook.authorFirstName + showBook.authorLastName}</p>
-    <p>Antal sidor: ${showBook.numberOfPages}</p>
-    <p>Utlånad: ${showBook.onLoan}</p>
-    <a href="http://localhost:3000/books/lend/${showBook.bookId}">Låna boken</a>`
+        let showBook = books.find((book) => book.bookId == req.params.bookid);
 
-    res.send(chosenBook + backToStartPage)
+        if (!showBook) {
+            return res.send("Det finns ingen bok med id " + req.params.bookid)
+        }
+
+        let chosenBook = `
+        <h2>Titel: ${showBook.title}</h2>
+        <p>Författare: ${showBook.authorFirstName + showBook.authorLastName}</p>
+        <p>Antal sidor: ${showBook.numberOfPages}</p>
+        <p>Utlånad: ${showBook.onLoan}</p>
+        <a href="http://localhost:3000/books/lend/${showBook.bookId}">Låna boken</a>`
+
+
+        res.send(chosenBook + backToStartPage)
+
+    })
 })
-
 
 
 //Link to lend the book. When clicked, the books status onLoan goes from false to true on start page
 router.get('/lend/:bookId', function (req, res) {
-    let showBook = libraryBooks.find((book) => book.bookId == req.params.bookId);
 
-    if (!showBook) {
-        return res.send("Det finns ingen bok med id " + showBook.bookId)
-    }
-    showBook.onLoan = true
-    res.send("Du har nu lånat boken " + showBook.title + backToStartPage)
+
+    fs.readFile("books.json", function (err, data) {
+
+        const books = JSON.parse(data)
+
+        let showBook = books.find((book) => book.bookId == req.params.bookId);
+
+        if (!showBook) {
+            return res.send("Det finns ingen bok med id " + showBook.bookId)
+        }
+
+        showBook.onLoan = true
+        res.send("Du har nu lånat boken " + showBook.title + backToStartPage)
+    })
+
 })
-
-
 
 
 //Create add new book page form
@@ -104,59 +138,38 @@ router.get('/addbook', function (req, res) {
 //Add new book with post
 router.post('/addbook', function (req, res) {
 
-    let newBook = {
-        "title": req.body.title,
-        "authorFirstName": req.body.authorFirstName,
-        "authorLastName": req.body.authorLastName,
-        "numberOfPages": req.body.numberOfPages,
-        "bookId": rand.generate(),
-        "onLoan": false
+    fs.readFile("books.json", function (err, data) {
+        if (err) {
+            console.log(err);
+        }
 
-    }
+        const books = JSON.parse(data)
 
-    libraryBooks.push(newBook)
+        let newBook = {
+            "title": req.body.title,
+            "authorFirstName": req.body.authorFirstName,
+            "authorLastName": req.body.authorLastName,
+            "numberOfPages": req.body.numberOfPages,
+            "bookId": rand.generate(),
+            "onLoan": false
 
-    res.send("Boken " + req.body.title + " har lagts till i biblioteket." + backToStartPage)
+        }
+
+        books.push(newBook)
+
+        fs.writeFile("books.json", JSON.stringify(books, null, 2), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+
+        res.send("Boken " + req.body.title + " har lagts till i biblioteket." + backToStartPage)
+
+    })
+
+
 
 })
-
-
-
-//Library books 
-let libraryBooks = [{
-        "title": "Harry Potter",
-        "authorFirstName": "J.K.",
-        "authorLastName": "Rowling",
-        "numberOfPages": 900,
-        "bookId": rand.generate(),
-        "onLoan": false
-    },
-    {
-        "title": "Where the Rain is Born",
-        "authorFirstName": "Anita",
-        "authorLastName": "Nair",
-        "numberOfPages": 297,
-        "bookId": rand.generate(),
-        "onLoan": false
-    },
-    {
-        "title": "Tears of the Giraffe",
-        "authorFirstName": "Alexander",
-        "authorLastName": "McCall Smith",
-        "numberOfPages": 217,
-        "bookId": rand.generate(),
-        "onLoan": false
-    },
-    {
-        "title": "The Kite Runner",
-        "authorFirstName": "Khaled",
-        "authorLastName": "Hosseini",
-        "numberOfPages": 371,
-        "bookId": rand.generate(),
-        "onLoan": false
-    }
-]
-
 
 
 
